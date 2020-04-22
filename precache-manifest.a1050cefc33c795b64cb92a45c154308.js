15b834e60ddfa8c2aa79d035c9fc42c438d3a7ad
bohdan88 <bohdan.martyniuk1488@gmail.com>
2020-04-22 11:39:47 -0400

    Updates

---
+++ precache-manifest.a1050cefc33c795b64cb92a45c154308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3192fb14b56640f0320cc6462291bd85",
+    "url": "/chart/index.html"
+  },
+  {
+    "revision": "dea0328059f867738d8d",
+    "url": "/chart/static/css/main.bba2217d.chunk.css"
+  },
+  {
+    "revision": "e69d6274f46218e4d6b5",
+    "url": "/chart/static/js/2.e5df78fe.chunk.js"
+  },
+  {
+    "revision": "7cd3ae0f9782082fc53eb06d2f25becd",
+    "url": "/chart/static/js/2.e5df78fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea0328059f867738d8d",
+    "url": "/chart/static/js/main.66e85b23.chunk.js"
+  },
+  {
+    "revision": "55c177e54033705542d2",
+    "url": "/chart/static/js/runtime-main.ac9cd548.js"
+  }
+]);
